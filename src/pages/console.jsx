aac7aa625947b300c023ed60788be4b3b0a8c504
amritendunath <amritendunath@gmail.com>
2025-08-21 21:59:59 +0530

    fixing_console_welcome_screen_3

--- src/pages/console.jsx
+++ src/pages/console.jsx
@@ -561,8 +561,8 @@ const ChatUI = () => {
         )}
         {/* Messages Area */}
         {/* sm:mt-8 md:mt-10 mb-[120px] sm:mb-[140px] md:mb-[150px] */}
-        <div className="flex-1 overflow-y-auto mt-[calc(60px+env(safe-area-inset-top))] pb-[calc(96px+env(safe-area-inset-bottom))]">
-          <div className="scrollbar  sm:p-4 sm:space-y-2 md:space-y-4 max-w-4xl mx-auto text-gray-200 text-[14px] pb-32">
+        <div className="flex-1 overflow-y-auto mt-10 md:mt-[100px] scrollbar">
+          <div className="sm:p-4 sm:space-y-2 md:space-y-4 max-w-4xl mx-auto text-gray-200 text-[14px] pb-32">
         {/* <div className="flex-1 overflow-auto scrollbar">
           <div className=" mt-10 sm:mt-12 md:mt-15 mb-[120px] sm:mb-[140px] md:mb-[150px]  p-2 sm:p-4 md:space-y-4 max-w-full sm:max-w-3xl md:max-w-4xl mx-auto text-gray-200 text-sm sm:text-sm md:text-[14px]"> */}
             {messages.map((msg, index) => (
