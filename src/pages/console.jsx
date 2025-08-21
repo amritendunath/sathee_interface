ac259ee528fa0cac0223cbc070639c965bb03eab
amritendunath <amritendunath@gmail.com>
2025-08-21 17:41:34 +0530

    fixing_console

--- src/pages/console.jsx
+++ src/pages/console.jsx
@@ -555,8 +555,8 @@ const ChatUI = () => {
         {/* <div className="flex-1 overflow-y-auto scrollbar">
         sm:mt-8 md:mt-10 mb-[120px] sm:mb-[140px] md:mb-[150px]
           <div className="mt-10 mb-[80px] sm:mb-[150px] sm:p-4 sm:space-y-2 md:space-y-4 max-w-4xl mx-auto text-gray-200 text-[14px]"> */}
-        <div className="flex-1 overflow-auto">
-          <div className=" mt-[30px] sm:mt-12 md:mt-15 mb-[120px] sm:mb-[140px] md:mb-[150px]  p-2 sm:p-4 md:space-y-4 max-w-full sm:max-w-3xl md:max-w-4xl mx-auto text-gray-200 text-xs sm:text-sm md:text-[14px]">
+        <div className="flex-1 overflow-auto scrollbar">
+          <div className=" mt-10 sm:mt-12 md:mt-15 mb-[120px] sm:mb-[140px] md:mb-[150px]  p-2 sm:p-4 md:space-y-4 max-w-full sm:max-w-3xl md:max-w-4xl mx-auto text-gray-200 text-xs sm:text-sm md:text-[14px]">
             {messages.map((msg, index) => (
               (typeof msg.message === 'string') ? (
                 <MessageBubble
